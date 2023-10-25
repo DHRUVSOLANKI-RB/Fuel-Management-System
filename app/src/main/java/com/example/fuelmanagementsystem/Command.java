ecf74cbf7d0a2ced7dd64ffd39ba31baee315403
DHRUVSOLANKI-RB <dhruv.solanki@railbit.in>
2023-10-25 11:57:34 +0530

    Android 12 support with Oneplus compatible added on 25-10-23 at 01:00 PM

--- app/src/main/java/com/example/fuelmanagementsystem/Command.java
+++ app/src/main/java/com/example/fuelmanagementsystem/Command.java
@@ -13,8 +13,10 @@ public class Command {
     public static final String nack_read = ("AA 02 80 04 7C 55");
     public static final String ack_read = ("AA 02 80 03 7D 55");
     public static final String nack_write = ("AA 02 81 04 7B 55");
-    public static final String emptytagresponse_read = ("AA 02 80 03 7D 55 AA 02 80 14 6C 55");
-    public static final String emptytagresponse_write = ("AA 02 81 03 7C 55 AA 02 81 14 6B 55");
+    /*public static final String emptytagresponse_read = ("AA 02 80 03 7D 55 AA 02 80 14 6C 55");*/
+    public static final String emptytagresponse_read = ("AA 02 80 14 6C 55");
+    /*public static final String emptytagresponse_write = ("AA 02 81 03 7C 55 AA 02 81 14 6B 55");*/
+    public static final String emptytagresponse_write = ("AA 02 81 14 6B 55");
     public static final String success_write = ("AA 02 81 03 7C 55 AA 02 81 13 6C 55");
     public static final String empty_data = "00 00 00 00 00 00 00 00 00 00 00 00";
 //    public static final String write_command_empty =("AA 11 01 00 00 00 00 00 00 00 00 00 00 00 00 FF FF FF 01 01 55");
--- app/src/main/java/com/example/fuelmanagementsystem/HttpUrl.java
+++ app/src/main/java/com/example/fuelmanagementsystem/HttpUrl.java
@@ -3,8 +3,9 @@ package com.example.fuelmanagementsystem;
 public class HttpUrl {
 
 
-//    public String  HttpURL_main = "https://3671-122-160-48-7.in.ngrok.io";
+    // public String  HttpURL_main = "https://3671-122-160-48-7.in.ngrok.io";
     public String  HttpURL_main = "http://www.fueleye.com";
+    /*public String  HttpURL_main = "http://3.7.237.203";*/
     public String HttpURL_login = HttpURL_main + "/android_login";
     public String HttpURL_fmsToken = HttpURL_main + "/e_fuel_login_req";
     public String HttpURL_vehicle_list = HttpURL_main + "/get_vehicle_list";
